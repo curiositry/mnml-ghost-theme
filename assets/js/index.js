f9102ecf46187ca4866011fd49087535a5182934
Omphalosskeptic <isa@automathic.org>
2014-11-01 12:16:03 -0700

    Added js to make pullquotes work with RSS, and use progressive enhancement

--- assets/js/index.js
+++ assets/js/index.js
@@ -87,3 +87,17 @@
 
     };
 })(jQuery);
+
+// Pullquotes
+$(document).ready(function() {
+  $('span.pullquote').each(function(index) {
+  var $parentParagraph = $(this).parent('p');
+  $parentParagraph.css('position', 'relative');
+  $(this).clone()
+    .addClass('pullquote--pulled')
+    .prependTo($parentParagraph);
+  });
+});
+
+
+
