af1ccd105cc364031617eae8117308a9f8618da7
Omphalosskeptic <isa@automathic.org>
2015-09-04 16:33:55 -0700

    Rewrote pullquote function to be jQuery free

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,43 @@
-// Pullquotes
-$(document).ready(function() {
-  $('span.pullquote').each(function(index) {
-  var $parentParagraph = $(this).parent('p');
-  $parentParagraph.css('position', 'relative');
-  $(this).clone()
-    .addClass('pullquote--pulled')
-    .prependTo($parentParagraph);
-  });
-});
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function previousElementSibling( elem ) {
+  do {
+    elem = elem.previousSibling;
+  } while ( elem && elem.nodeType !== 1 );
+  return elem;
+}
+
+function pullPullQuotes(){
+  var pullquotes = document.getElementsByClassName('pullquote');
+  for (var i = 0; i < pullquotes.length; i++) {
+    var el = pullquotes[i];
+    for (var i = 0; i < 10; i++){
+      if (el.parentNode.nodeName == "P"){
+        var parentParagraph = el.parentNode;
+        break;
+      }
+    }
+    if (previousElementSibling(parentParagraph).nodeName == "P"){
+      parentParagraph = previousElementSibling(parentParagraph);
+    }
+    var newEl = el.cloneNode(true);
+    newEl.classList.add('pullquote--pulled');
+    newEl.classList.remove('pullquote');
+    parentParagraph.insertBefore(newEl, parentParagraph.firstChild);
+  }
+}
+
+ready(pullPullQuotes());
+
+
+
+
 
 
 
