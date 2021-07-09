f469935ae9318621bbda16bdcecd9200667bce89
Curiositry <isa@automathic.org>
2021-07-09 14:28:16 -0700

    Responsive YouTube embeds

--- assets/js/index.js
+++ assets/js/index.js
@@ -99,3 +99,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
     URLSearchParams.delete(stripe);
   }
 });
+
+
+// Vanilla version of FitVids
+// Licencened under WTFPL
+// source: https://css-tricks.com/fluid-width-video/
+
+// List of Video Vendors embeds you want to support
+var players = ['iframe[src*="youtube.com"]', 'iframe[src*="youtube-nocookie.com"]', 'iframe[src*="vimeo.com"]'];
+
+// Select videos
+var fitVids = document.querySelectorAll(players.join(","));
+
+// If there are videos on the page...
+if (fitVids.length) {
+  // Loop through videos
+  for (var i = 0; i < fitVids.length; i++) {
+    // Get Video Information
+    var fitVid = fitVids[i];
+    var width = fitVid.getAttribute("width");
+    var height = fitVid.getAttribute("height");
+    var aspectRatio = height / width;
+    var parentDiv = fitVid.parentNode;
+
+    // Wrap it in a DIV
+    var div = document.createElement("div");
+    div.className = "video-wrapper";
+    div.style.paddingBottom = aspectRatio * 100 + "%";
+    parentDiv.insertBefore(div, fitVid);
+    fitVid.remove();
+    div.appendChild(fitVid);
+
+    // Clear height/width from fitVid
+    fitVid.removeAttribute("height");
+    fitVid.removeAttribute("width");
+  }
+}
